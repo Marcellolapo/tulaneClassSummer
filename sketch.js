9dd289dd300f4bc4d17d3fa3f010060afb19d714
Marcellolapo <simbaisthebest77@gmail.com>
2024-07-02 15:36:21 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,75 @@
+function setup() {
+  createCanvas(800, 600);
+  rectMode(CENTER)
+}
+
+function draw() {
+  background(3, 215, 252);
+  
+  fill(3, 252, 61)
+  strokeWeight(1)
+  stroke(0)
+  
+  rect(400,550,800,100)
+  fill(252, 219, 3)
+  rect(400,350,350,300)
+  
+  fill(97, 68, 36)
+  triangle(225,200,400,50,575,200)
+  
+  strokeWeight(40)
+  stroke(225)
+  point(50,100)
+  point(130,100)
+  point(70,100)
+  point(150,100)
+  point(90,100)
+  point(170,100)
+  point(110,100)
+  point(40,90)
+  point(30,80)
+  point(30,70)
+  point(40,60)
+  point(60,60)
+  point(80,60)
+  point(100,60)
+  point(120,60)
+  point(140,60)
+  point(160,60)
+  point(170,70)
+  point(180,80)
+  point(190,90)
+  point(185,95)
+ 
+  fill(225)
+  ellipse(100,90,100,10)
+  
+  stroke(255, 238, 5)
+  fill(255, 238, 5)
+  circle(775,25,50)
+  
+  strokeWeight(10)
+  line(700,200,750,100)
+  line(650,150,720,75)
+  line(620,100,710,40)
+  
+  fill(5, 206, 232)
+  stroke(5, 206, 232)
+  rect(300,300,50,50)
+  
+  rect(500,300,50,50)
+  
+  fill(97, 68, 36)
+  stroke(97, 68, 36)
+  rect(400,444,50,100)
+  
+  fill(225)
+  stroke(225)
+  point(420,450)
+  
+  strokeWeight(6)
+  line(300,273,300,328)
+  line(500,273,500,328)
+  line(273,300,328,300)
+  line(473,300,528,300)
+}
